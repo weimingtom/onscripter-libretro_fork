5c4914b3aa4205de758ab9a5fefea1d600884c98
weimingtom <weimingtom@qq.com>
2024-11-23 04:48:45 +0800

    ndk-build don't use APP_ABI := all, build one per time

--- onscripter-libretro/jni/Application.mk
+++ onscripter-libretro/jni/Application.mk
@@ -1,4 +1,6 @@
-#APP_ABI := all
+#dont' use APP_ABI := all, build one per time
+
 APP_ABI := armeabi-v7a
 #APP_ABI := arm64-v8a
+
 APP_STL := c++_static
--- onsyuri_libretro/jni/Application.mk
+++ onsyuri_libretro/jni/Application.mk
@@ -1,6 +1,7 @@
 APP_PLATFORM := android-13
 
-#APP_ABI := all
+#dont' use APP_ABI := all, build one per time
+
 APP_ABI := armeabi-v7a
 #APP_ABI := arm64-v8a
 
